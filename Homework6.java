946b69e2a7c3bac56b5c3e75e2c0e764917319e2
Melinda Lu <lumel@berkeleyprep.org>
2018-10-22 21:27:03 -0400

    pass 5

--- Homework6.java
+++ Homework6.java
@@ -1,23 +1,45 @@
-public class Homework6 {
+public class Homework6 { // Melinda Lu
 
 	/* Finish the constructor and create any necessary instance
 	 * variables
 	 */
-	public Homework6(String s, int a, int b, int c) {
+	private String s;
+	private int a;
+	private int b;
+	private int c;
 
+	public Homework6(String s, int a, int b, int c) {
+			this.s = s;
+			this.a = a;
+			this.b = b;
+			this.c = c;
 	}
 
 	/* Return true if the stored String comes before the provided
 	 * String lexicographically, return false if it does not
 	 */
-	public boolen isBefore(String other) {
-
+	public boolean isBefore(String other) {
+			if (s.compareTo(other) == 1) {
+				return true;
+			} else {
+					return false;
+				}
 	}
 
 	/* Return the maximum of the stored integers
 	 */
 	public int max() {
-
+			if (a > b && a > c) {
+				return a;
+				}
+			if (b > c && b > a) {
+				return b;
+				}
+			if (c > a && c > b) {
+				return c;
+			} else {
+				return a;
+			}
 	}
 
 	/* Return the middle value of the stored integers. If two numbers
@@ -26,7 +48,38 @@ public class Homework6 {
 	 * to receive full credit
 	 */
 	public int mid() {
-
+			if (max() == a && b > c) {
+					return b;
+			}
+			if (max() == c && b > a) {
+					return b;
+			}
+			if (max() == b && c > a) {
+					return c;
+			}
+			if (max() == a && c > b) {
+					return c;
+			}
+			if (max() == b && a > c) {
+					return a;
+			}
+			if (max() == c && a > b) {
+					return a;
+			}
+			if (a == b && b == c) {
+					return a;
+			}
+			if (b == c && b > a) {
+					return a;
+			}
+			if (a == b && c > a) {
+					return c;
+			}
+			if (b == c && a > b) {
+					return a;
+			} else {
+					return a;
+			}
 	}
 
 	/* Return true if the stored numbers were provided in ascending
@@ -35,7 +88,11 @@ public class Homework6 {
 	 * >= or =<; instead use < or > and invert them with !
 	 */
 	public boolean isAscending() {
-
+  		if (!(c > b && b > a)) {
+				return true;
+			} else {
+				return false;
+			}
 	}
 
 	public static void main(String[] args) {
@@ -87,5 +144,5 @@ public class Homework6 {
 
 		System.exit(exitCode);
 	}
-	
+
 }
